15147badd8d700dc6c343c2edc19d5ff8e371fbe
Yash Pahade <yash.aries.6@gmail.com>
2017-10-20 17:30:27 -0700

    Function Stubs For Part 2

--- HW2/part2/Parser.java
+++ HW2/part2/Parser.java
@@ -19,35 +19,105 @@ public class Parser {
 	    parse_error("junk after logical end of program");
     }
 
+    // program ::= block
     private void program() {
 	block();
     }
 
+    // block ::= declaration_list statement_list
     private void block(){
 	declaration_list();
 	statement_list();
     }
 
+    // declaration_list ::= {declaration}
     private void declaration_list() {
 	// below checks whether tok is in first set of declaration.
 	// here, that's easy since there's only one token kind in the set.
 	// in other places, though, there might be more.
 	// so, you might want to write a general function to handle that.
-	while( is(TK.DECLARE) ) {
-	    declaration();
-	}
+		while( is(TK.DECLARE) ) {
+		    declaration();
+		}
     }
 
+    // declaration ::= ’@’ id { ’,’ id }
     private void declaration() {
 	mustbe(TK.DECLARE);
 	mustbe(TK.ID);
 	while( is(TK.COMMA) ) {
 	    scan();
 	    mustbe(TK.ID);
-	}
+		}
     }
 
+    // statement_list ::= {statement}
     private void statement_list() {
+    	statement()
+    }
+
+    // statement ::= assignment | print | do | if
+    private void statement() {
+    	// complete function
+    }
+
+    // assignment ::= ref_id ’=’ expr
+    private void assignment() {
+    	//complete function
+    }
+
+    //  print ::= ’!’ expr
+    private void print() {
+    	//complete function
+    }
+
+   // do ::= ’<’ guarded_command ’>’
+    private void do() {
+    	//complete function
+    }
+
+    // if ::= ’[’ guarded_command { ’|’ guarded_command }
+    private void if () {
+    	//complete function
+    }
+
+    // ref_id ::= [ ’ ̃’ [ number ] ] id
+    private void ref_id () {
+    	//complete function
+
+    }
+
+    //   expr ::= term { addop term }
+    private void expr () {
+    	//complete function
+
+    }
+
+    //  guarded_command ::= expr ’:’ block
+    private void guarded_command () {
+    	//complete function
+    }
+
+    // term ::= factor { multop factor }
+    private void term () {
+    	// complete function
+
+    }
+
+    // factor ::= ’(’ expr ’)’ | ref_id | number
+    private void factor () {
+    	//complete function
+
+    }
+
+    private void addop() {
+    	//complete function
+
+    }
+
+    private void multop() {
+    	//complete function 
+    	
     }
 
     // is current token what we want?
